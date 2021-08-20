ae4ccd5f3af74ffa324c1f23732045873a47f6f6
ygormagalhaes97 <88738603+ygormagalhaes97@users.noreply.github.com>
2021-08-19 22:07:30 -0300

    JavaScript
    Criado funções de cada coisa pode ser usado no javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-alert("minha pagina")
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar" ;
+}
+function redirecionar(){
+    window.open("https://www.youtube.com/");
+    window.location.href = "https://www.youtube.com/";
+}
+function mouse(elemento){
+    //document.getElementById("over").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+function voltar(elemento){
+    //document.getElementById("over").innerHTML = "Passe o mouse aki";
+    elemento.innerHTML = "Passe o mouse aki";
+}
+function load(){
+    alert("pagina carregada");
+}
+function change(elemento){
+    console.log(elemento.value);
+}
